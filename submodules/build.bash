c7d728b624a7d2d22c8ab704e88fdecfd2b872e7
Leo Conforti <leo@leoconforti.us>
2024-02-19 02:27:15 +0000

    save

--- submodules/build.bash
+++ submodules/build.bash
@@ -3,11 +3,11 @@
 (cd ./wireguard-go && make clean)
 
 # amd64 prebuilds
-(cd ./wireguard-go && GOOS=linux GOARCH=amd64 make && mv wireguard-go ../../prebuild/linux-amd64-wireguard-go)
-(cd ./wireguard-go && GOOS=darwin GOARCH=amd64 make && mv wireguard-go ../../prebuild/darwin-amd64-wireguard-go)
-(cd ./wireguard-go && GOOS=windows GOARCH=amd64 make && mv wireguard-go ../../prebuild/windows-amd64-wireguard-go)
+(cd ./wireguard-go && GOOS=linux GOARCH=amd64 make && mv wireguard-go ../../build/linux-amd64-wireguard-go)
+(cd ./wireguard-go && GOOS=darwin GOARCH=amd64 make && mv wireguard-go ../../build/darwin-amd64-wireguard-go)
+(cd ./wireguard-go && GOOS=windows GOARCH=amd64 make && mv wireguard-go ../../build/windows-amd64-wireguard-go)
 
 # arm64 prebuilds
-(cd ./wireguard-go && GOOS=linux GOARCH=arm64 make && mv wireguard-go ../../prebuild/linux-arm64-wireguard-go)
-(cd ./wireguard-go && GOOS=darwin GOARCH=arm64 make && mv wireguard-go ../../prebuild/darwin-arm64-wireguard-go)
-(cd ./wireguard-go && GOOS=windows GOARCH=arm64 make && mv wireguard-go ../../prebuild/windows-arm64-wireguard-go)
+(cd ./wireguard-go && GOOS=linux GOARCH=arm64 make && mv wireguard-go ../../build/linux-arm64-wireguard-go)
+(cd ./wireguard-go && GOOS=darwin GOARCH=arm64 make && mv wireguard-go ../../build/darwin-arm64-wireguard-go)
+(cd ./wireguard-go && GOOS=windows GOARCH=arm64 make && mv wireguard-go ../../build/windows-arm64-wireguard-go)
