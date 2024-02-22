1df9b4bd728d259f822e88e9c598f133cab0da07
Leo Conforti <leo@leoconforti.us>
2024-02-22 00:36:37 +0000

    add js build script

--- submodules/build.cjs
+++ submodules/build.cjs
@@ -1,4 +1,5 @@
 module.exports.runAsync = async function () {
+    console.log("[build:wireguard-go] Building wireguard-go...");
     const exec = await import("execa");
     exec.execaCommandSync("./build.bash", { cwd: __dirname });
 };
