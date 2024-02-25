0d23452632b38a18ed44b7bbffec9c329e14790c
Leo Conforti <leo@leoconforti.us>
2024-02-25 07:40:54 +0000

    add basic test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,21 @@
+import * as Platform from "@effect/platform-node";
+import * as Effect from "effect/Effect";
+
+import * as Wireguard from "../src/index.js";
+
 describe("Wireguard tests", () => {
-    it.skip("should", async () => {});
+    it("should", async () =>
+        Effect.gen(function* (λ) {
+            const [hubConfig, spokeConfigs] = Wireguard.WireguardInterfaceConfig.generateHubSpokeConfigs(
+                "10.0.0.1:12345",
+                ["10.0.0.2:23456", "10.0.0.3:4598"]
+            );
+
+            yield* λ(hubConfig.writeToFile("docs/hub.conf"));
+            for (const [index, spokeConfig] of spokeConfigs.entries()) {
+                yield* λ(spokeConfig.writeToFile(`docs/spoke${index}.conf`));
+            }
+        })
+            .pipe(Effect.provide(Platform.NodeContext.layer))
+            .pipe(Effect.runPromise));
 });
