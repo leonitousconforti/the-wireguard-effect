adfab0b89f951f0fe134f8b50dd8e0048158e6b7
Leo Conforti <leo@leoconforti.us>
2024-03-01 23:00:58 +0000

    install api-extractor package

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,7 +9,7 @@ source /home/vscode/.bashrc
 pnpm config set store-dir $PNPM_HOME/store
 
 echo "📦 Installing global dependencies..."
-npm install -g @devcontainers/cli tsx
+npm install -g @devcontainers/cli tsx @microsoft/api-extractor
 
 echo "📦 Installing repo dependencies..."
 pnpm install
