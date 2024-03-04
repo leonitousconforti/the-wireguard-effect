655b04030172775db9b8f1bb65644cb12096bfe5
Leo Conforti <leo@leoconforti.us>
2024-03-04 06:25:37 +0000

    save

--- actions/connect.ts
+++ actions/connect.ts
@@ -92,8 +92,8 @@ const waitForResponse = Effect.gen(function* (λ) {
     // Still waiting for a connection response
     yield* λ(Effect.fail(new Error("Still waiting for a connection response")));
 })
-    .pipe(Effect.catchAll(Console.log))
-    .pipe(Effect.catchAllDefect(Console.log));
+    .pipe(Effect.tapError(Console.log))
+    .pipe(Effect.tapDefect(Console.log));
 
 Effect.suspend(() => uploadConnectionRequestArtifact).pipe(
     Effect.andThen(
