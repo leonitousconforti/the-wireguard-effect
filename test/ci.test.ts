fe8515c3fed381d3e4b1fbe24fc7b4dfbe0b9f57
Leo Conforti <leo@leoconforti.us>
2024-03-04 01:21:57 +0000

    save

--- test/ci.test.ts
+++ test/ci.test.ts
@@ -4,7 +4,12 @@ import * as execa from "execa";
 describe("The bundled github actions should not have been been modified after building in CI", () => {
     it("should not have been modified", () => {
         if (ciInfo.isCI) {
-            execa.execaCommandSync("git diff --exit-code .github/", { stdio: "inherit" });
+            execa.execaCommandSync(
+                "git diff --exit-code .github/actions/workflow-level-service/connect/connect.bundle.mjs .github/actions/workflow-level-service/expose/expose.bundle.mjs",
+                {
+                    stdio: "inherit",
+                }
+            );
         } else {
             expect(true).toBe(true);
         }
