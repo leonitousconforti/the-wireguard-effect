9f71212a7037b8f125ba885f14f9827b6788181f
Leo Conforti <leo@leoconforti.us>
2024-03-14 20:37:50 +0000

    add wintun

--- submodules/build.bash
+++ submodules/build.bash
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-# amd64 prebuilds
+# amd64 wireguard-go prebuilds
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=linux GOARCH=amd64 make && mv wireguard-go ../../build/linux-amd64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=darwin GOARCH=amd64 make && mv wireguard-go ../../build/darwin-amd64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=windows GOARCH=amd64 make && mv wireguard-go ../../build/win32-amd64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=freebsd GOARCH=amd64 make && mv wireguard-go ../../build/freebsd-amd64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=openbsd GOARCH=amd64 make && mv wireguard-go ../../build/openbsd-amd64-wireguard-go)
 
-# arm64 prebuilds
+# arm64 wireguard-go prebuilds
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=linux GOARCH=arm64 make && mv wireguard-go ../../build/linux-arm64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=darwin GOARCH=arm64 make && mv wireguard-go ../../build/darwin-arm64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=windows GOARCH=arm64 make && mv wireguard-go ../../build/win32-arm64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=freebsd GOARCH=arm64 make && mv wireguard-go ../../build/freebsd-arm64-wireguard-go)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=openbsd GOARCH=arm64 make && mv wireguard-go ../../build/openbsd-arm64-wireguard-go)
+
+# wintun prebuilds
+(cd ./wintun && cp wintun-amd64.dll ../../build/win32-amd64-wintun.dll)
+(cd ./wintun && cp wintun-arm64.dll ../../build/win32-arm64-wintun.dll)
