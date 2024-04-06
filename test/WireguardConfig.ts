0269c5b02e3a26ac4870bc312fe199632639bba9
Leo Conforti <leo@leoconforti.us>
2024-04-06 02:56:57 +0000

    update test

--- test/WireguardConfig.ts
+++ test/WireguardConfig.ts
@@ -4,30 +4,46 @@ import * as PlatformNode from "@effect/platform-node";
 import * as Schema from "@effect/schema/Schema";
 import * as Chunk from "effect/Chunk";
 import * as Effect from "effect/Effect";
+import * as Function from "effect/Function";
 import * as Sink from "effect/Sink";
 import * as Stream from "effect/Stream";
+import * as Tuple from "effect/Tuple";
+
 import * as InternetSchemas from "the-wireguard-effect/InternetSchemas";
 import * as WireguardConfig from "the-wireguard-effect/WireguardConfig";
 
 describe("WireguardConfig", () => {
     it("should generate p2p configs", async () =>
         await Effect.gen(function* (λ) {
-            const alice = "1.1.1.1:51820" as const;
-            const bob = "2.2.2.2:51820" as const;
-            const network = "192.168.0.1/24" as const;
+            const aliceEndpoint = "1.1.1.1:51820" as const;
+            const bobEndpoint = "2.2.2.2:51820" as const;
+            const networkCidr = "192.168.0.1/24" as const;
 
-            const cidrBlock = yield* λ(Schema.decode(InternetSchemas.CidrBlock)(network));
-            const ips = yield* λ(cidrBlock.range.pipe(Stream.run(Sink.collectAllN(2))).pipe(Effect.map(Chunk.toArray)));
+            const cidrBlock = yield* λ(Schema.decode(InternetSchemas.CidrBlockFromString)(networkCidr));
+            const ips = yield* λ(
+                Function.pipe(
+                    cidrBlock.range,
+                    Stream.map(({ ip }) => ip),
+                    Stream.run(Sink.collectAllN(2)),
+                    Effect.map(Chunk.toArray)
+                )
+            );
+
+            const aliceSetupData = Tuple.make(aliceEndpoint, ips[0]);
+            const bobSetupData = Tuple.make(bobEndpoint, ips[1]);
 
             const [aliceConfig, bobConfig] = yield* λ(
-                WireguardConfig.WireguardConfig.generateP2PConfigs([alice, ips[0]], [bob, ips[1]]),
+                WireguardConfig.WireguardConfig.generateP2PConfigs({
+                    aliceData: aliceSetupData,
+                    bobData: bobSetupData,
+                })
             );
 
             yield* λ(aliceConfig.writeToFile("alice.conf"));
             yield* λ(bobConfig.writeToFile("bob.conf"));
 
-            const aliceConfig2 = yield* λ(WireguardConfig.WireguardConfig.fromConfigFile("alice.conf"));
-            const bobConfig2 = yield* λ(WireguardConfig.WireguardConfig.fromConfigFile("bob.conf"));
+            // const aliceConfig2 = yield* λ(WireguardConfig.WireguardConfig.fromConfigFile("alice.conf"));
+            // const bobConfig2 = yield* λ(WireguardConfig.WireguardConfig.fromConfigFile("bob.conf"));
         })
             .pipe(Effect.provide(PlatformNode.NodeContext.layer))
             .pipe(Effect.runPromise));
