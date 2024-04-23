90a70f87a999ca65b5ca63033b9262a261a50592
Leo Conforti <leo@leoconforti.us>
2024-04-23 20:37:13 +0000

    update submodule building

--- submodules/build.bash
+++ submodules/build.bash
@@ -3,6 +3,9 @@
 set -euxo pipefail
 mkdir -p ../dist/prebuilds
 
+# osxcross for cross compiling wg to darwin
+(cd ./osxcross && sudo UNATTENDED=1 TARGET_DIR=/usr/local/osxcross ./build.sh)
+
 # amd64 wireguard-go prebuilds
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=windows GOARCH=amd64 make && mv wireguard-go ../../dist/prebuilds/win32-amd64-wireguard-go.exe && chmod +x ../../dist/prebuilds/win32-amd64-wireguard-go.exe)
 (cd ./wireguard-go && make clean && GOFLAGS="-buildvcs=false" GOOS=linux GOARCH=amd64 make && mv wireguard-go ../../dist/prebuilds/linux-amd64-wireguard-go && chmod +x ../../dist/prebuilds/linux-amd64-wireguard-go)
@@ -30,10 +33,10 @@ sudo ldconfig
 (cd ./nvlist && git reset --hard && git clean --force -d)
 
 # wg-quick prebuilds
-(cd ./wireguard-tools/src && make clean && make PLATFORM=linux && cp ./wg ../../../dist/prebuilds/linux-wg && chmod +x ../../../dist/prebuilds/linux-wg)
-(cd ./wireguard-tools/src && make clean && make PLATFORM=darwin && cp ./wg ../../../dist/prebuilds/darwin-wg && chmod +x ../../../dist/prebuilds/darwin-wg)
-(cd ./wireguard-tools/src && make clean && make PLATFORM=freebsd && cp ./wg ../../../dist/prebuilds/freebsd-wg && chmod +x ../../../dist/prebuilds/freebsd-wg)
-(cd ./wireguard-tools/src && make clean && make PLATFORM=openbsd && cp ./wg ../../../dist/prebuilds/openbsd-wg && chmod +x ../../../dist/prebuilds/openbsd-wg)
+(cd ./wireguard-tools/src && make clean && PLATFORM=linux make && cp ./wg ../../../dist/prebuilds/linux-wg && chmod +x ../../../dist/prebuilds/linux-wg)
+(cd ./wireguard-tools/src && make clean && PLATFORM=darwin CC=/usr/local/osxcross/bin/o64-clang make && cp ./wg ../../../dist/prebuilds/darwin-wg && chmod +x ../../../dist/prebuilds/darwin-wg)
+(cd ./wireguard-tools/src && make clean && PLATFORM=freebsd make && cp ./wg ../../../dist/prebuilds/freebsd-wg && chmod +x ../../../dist/prebuilds/freebsd-wg)
+(cd ./wireguard-tools/src && make clean && PLATFORM=openbsd make && cp ./wg ../../../dist/prebuilds/openbsd-wg && chmod +x ../../../dist/prebuilds/openbsd-wg)
 (cd ./wireguard-tools && git apply ../../patches/wg-quick-linux.patch && cp src/wg-quick/linux.bash ../../dist/prebuilds/linux-wg-quick && chmod +x ../../dist/prebuilds/linux-wg-quick)
 (cd ./wireguard-tools && git apply ../../patches/wg-quick-darwin.patch && cp src/wg-quick/darwin.bash ../../dist/prebuilds/darwin-wg-quick && chmod +x ../../dist/prebuilds/darwin-wg-quick)
 (cd ./wireguard-tools && git apply ../../patches/wg-quick-freebsd.patch && cp src/wg-quick/freebsd.bash ../../dist/prebuilds/freebsd-wg-quick && chmod +x ../../dist/prebuilds/freebsd-wg-quick)
