cc227b4e0f114435dc73ce15f95e54fb7afa2d40
Leo Conforti <leo@leoconforti.us>
2024-04-25 15:00:47 +0000

    update build

--- submodules/build.bash
+++ submodules/build.bash
@@ -35,7 +35,7 @@ sudo ldconfig
 
 # wg-quick prebuilds
 (cd ./wireguard-tools/src && make clean && PLATFORM=linux make && cp ./wg ../../../dist/prebuilds/linux-wg && chmod +x ../../../dist/prebuilds/linux-wg)
-(cd ./wireguard-tools/src && make clean && PLATFORM=darwin CC=/usr/local/osxcross/bin/o64-clang make && cp ./wg ../../../dist/prebuilds/darwin-wg && chmod +x ../../../dist/prebuilds/darwin-wg)
+(cd ./wireguard-tools/src && make clean && PLATFORM=darwin V=1 CC=/usr/local/osxcross/bin/o64-clang make && cp ./wg ../../../dist/prebuilds/darwin-wg && chmod +x ../../../dist/prebuilds/darwin-wg)
 (cd ./wireguard-tools/src && make clean && PLATFORM=freebsd make && cp ./wg ../../../dist/prebuilds/freebsd-wg && chmod +x ../../../dist/prebuilds/freebsd-wg)
 (cd ./wireguard-tools/src && make clean && PLATFORM=openbsd make && cp ./wg ../../../dist/prebuilds/openbsd-wg && chmod +x ../../../dist/prebuilds/openbsd-wg)
 (cd ./wireguard-tools && git apply ../../patches/wg-quick-linux.patch && cp src/wg-quick/linux.bash ../../dist/prebuilds/linux-wg-quick && chmod +x ../../dist/prebuilds/linux-wg-quick)
