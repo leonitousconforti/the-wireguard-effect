365d061dde7539b945d4950c3c8bee335ace1fe4
Leo Conforti <leo@leoconforti.us>
2024-06-15 01:16:36 +0000

    save

---
+++ e2e/run.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import * as exec from "node:child_process";
+import * as url from "node:url";
+
+const runDockerE2E = (folder) => {
+    const cwd = url.fileURLToPath(new URL(folder, import.meta.url));
+    exec.execSync("tsx generate-configs.ts", { cwd, stdio: "inherit" });
+    exec.execSync("docker compose build", { cwd, stdio: "inherit" });
+    exec.execSync("docker compose up --abort-on-container-failure", { cwd, stdio: "inherit" });
+    exec.execSync("docker compose down", { cwd, stdio: "inherit" });
+};
+
+runDockerE2E("./generate-server-to-server-access");
