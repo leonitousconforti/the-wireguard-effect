efa4d05b24d442f277fe1b6a37b41837018ea977
Leo Conforti <leo@leoconforti.us>
2024-06-16 05:10:55 +0000

    another e2e test

---
+++ e2e/generate-lan-to-lan-access/A-alice-client-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ip route add 10.0.0.0/24 via 10.0.1.2
+wg-quick up wg0
+ping 192.168.10.1 -c 5
+ping 192.168.10.2 -c 5
+ping 10.0.2.2 -c 5
+ping 10.0.2.3 -c 5
+ping 10.0.2.4 -c 5
+sleep 5s
