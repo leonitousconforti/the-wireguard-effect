c97a8b9371c148278f8e5126cbd546a9e1a3656f
Leo Conforti <leo@leoconforti.us>
2024-06-24 16:55:36 +0000

    update e2e test

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "@effect/vitest";
+import { describe, it } from "@effect/vitest";
 
 import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as NodeHttp from "@effect/platform-node/NodeHttpClient";
@@ -14,7 +14,7 @@ import * as WireguardServer from "the-wireguard-effect/WireguardServer";
 
 const portConfig = Config.number("WIREGUARD_DEMO_PORT").pipe(Config.withDefault(42912));
 const hostConfig = Config.string("WIREGUARD_DEMO_HOST").pipe(Config.withDefault("demo.wireguard.com"));
-const hiddenPageUrlConfig = Config.string("HIDDEN_PAGE").pipe(Config.withDefault("http://192.168.4.1:80"));
+// const hiddenPageUrlConfig = Config.string("HIDDEN_PAGE").pipe(Config.withDefault("http://192.168.4.1:80"));
 
 const WireguardControlLive = Layer.sync(WireguardControl.WireguardControl, () =>
     WireguardControl.makeBundledWgQuickLayer({ sudo: process.platform !== "linux" })
@@ -27,7 +27,7 @@ describe("wireguard e2e test using demo.wireguard.com", () => {
             Effect.gen(function* (λ) {
                 const host = yield* λ(hostConfig);
                 const port = yield* λ(portConfig);
-                const hiddenPageUrl = yield* λ(hiddenPageUrlConfig);
+                // const hiddenPageUrl = yield* λ(hiddenPageUrlConfig);
                 const config = yield* λ(WireguardServer.requestWireguardDemoConfig({ host, port }));
                 yield* λ(config.upScoped());
                 yield* Console.log("Interface is up");
@@ -36,9 +36,9 @@ describe("wireguard e2e test using demo.wireguard.com", () => {
                 // if (host === "demo.wireguard.com") yield* λ(WireguardServer.requestGoogle);
                 // yield* Console.log("Connected to https://google.com");
 
-                const hiddenPage = yield* λ(WireguardServer.requestHiddenPage(hiddenPageUrl));
-                yield* Console.log("Connected to hidden page");
-                expect(hiddenPage).toMatchSnapshot();
+                // const hiddenPage = yield* λ(WireguardServer.requestHiddenPage(hiddenPageUrl));
+                // yield* Console.log("Connected to hidden page");
+                // expect(hiddenPage).toMatchSnapshot();
             })
                 .pipe(Effect.scoped)
                 .pipe(Effect.provide(NodeHttp.layer))
