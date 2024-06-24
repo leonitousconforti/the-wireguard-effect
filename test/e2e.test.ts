619060143db2b3ac5e8c688e15d44e9f52eb0dc8
Leo Conforti <leo@leoconforti.us>
2024-06-24 17:45:42 +0000

    increase e2e test timeout

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -4,7 +4,9 @@ import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as NodeHttp from "@effect/platform-node/NodeHttpClient";
 import * as Config from "effect/Config";
 import * as Console from "effect/Console";
+import * as Duration from "effect/Duration";
 import * as Effect from "effect/Effect";
+import * as Function from "effect/Function";
 import * as Layer from "effect/Layer";
 
 import * as WireguardControl from "the-wireguard-effect/WireguardControl";
@@ -19,25 +21,28 @@ const WireguardControlLive = Layer.sync(WireguardControl.WireguardControl, () =>
 );
 
 describe("wireguard e2e test using demo.wireguard.com", () => {
-    it.scoped("Should be able to connect to the demo server", () =>
-        Effect.gen(function* (λ) {
-            const host = yield* λ(hostConfig);
-            const port = yield* λ(portConfig);
-            const hiddenPageUrl = yield* λ(hiddenPageUrlConfig);
-            const config = yield* λ(WireguardServer.requestWireguardDemoConfig({ host, port }));
+    it.scoped(
+        "Should be able to connect to the demo server",
+        () =>
+            Effect.gen(function* (λ) {
+                const host = yield* λ(hostConfig);
+                const port = yield* λ(portConfig);
+                const hiddenPageUrl = yield* λ(hiddenPageUrlConfig);
+                const config = yield* λ(WireguardServer.requestWireguardDemoConfig({ host, port }));
 
-            yield* λ(config.upScoped());
-            yield* Console.log("Interface is up");
+                yield* λ(config.upScoped());
+                yield* Console.log("Interface is up");
 
-            if (host === "demo.wireguard.com") yield* λ(WireguardServer.requestGoogle);
-            yield* Console.log("Connected to https://google.com");
+                if (host === "demo.wireguard.com") yield* λ(WireguardServer.requestGoogle);
+                yield* Console.log("Connected to https://google.com");
 
-            const hiddenPage = yield* λ(WireguardServer.requestHiddenPage(hiddenPageUrl));
-            yield* Console.log("Connected to hidden page");
-            expect(hiddenPage).toMatchSnapshot();
-        })
-            .pipe(Effect.provide(NodeHttp.layer))
-            .pipe(Effect.provide(NodeContext.layer))
-            .pipe(Effect.provide(WireguardControlLive))
+                const hiddenPage = yield* λ(WireguardServer.requestHiddenPage(hiddenPageUrl));
+                yield* Console.log("Connected to hidden page");
+                expect(hiddenPage).toMatchSnapshot();
+            })
+                .pipe(Effect.provide(NodeHttp.layer))
+                .pipe(Effect.provide(NodeContext.layer))
+                .pipe(Effect.provide(WireguardControlLive)),
+        Function.pipe(1, Duration.minutes, Duration.toMillis)
     );
 });
