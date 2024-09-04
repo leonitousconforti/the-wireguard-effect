4a638a74740f73fd5400ddf914527889a7a4d8d0
Leo Conforti <leo@leoconforti.us>
2024-09-04 13:42:33 +0000

    attempt to make e2e tests fully reliable

--- e2e/generate-lan-hub-and-spoke-access/C-charlie-client-tests.sh
+++ e2e/generate-lan-hub-and-spoke-access/C-charlie-client-tests.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 ip route add 10.0.0.0/24 via 10.0.3.2
 wg-quick up wg0
+sleep 3s
 ping 192.168.10.1 -c 3
 ping 192.168.10.2 -c 3
 ping 192.168.10.3 -c 3
 ping 192.168.10.100 -c 3
 ping 10.0.1.4 -c 3
 ping 10.0.1.5 -c 3
-sleep 5s
+sleep 8s
