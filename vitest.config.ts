009aba78d1fccc536222961dfb862167a31af980
Leo Conforti <leoconforti@ltgk.net>
2025-02-03 19:11:17 +0000

    fix vitest configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,5 @@
-/// <reference types="vitest" />
-
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
