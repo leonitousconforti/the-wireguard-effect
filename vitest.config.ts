6b1c86ea7664c1bf0dc61983dcbf958cb3a60ec1
Leo Conforti <leoconforti@ltgk.net>
2025-03-06 19:21:19 +0000

    fix setup files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         include: ["./test/**/*.test.ts"],
-        globalSetup: "./test/vitest.setup.ts",
+        setupFiles: ["./test/vitest.setup.ts"],
         coverage: { provider: "v8", include: ["src/**/*.ts"], reporter: ["cobertura", "text"] },
         reporters: ["default", "hanging-process", ["junit", { outputFile: "./coverage/junit.xml" }]],
     },
